c59f34218e135a3719bacc2b17c0cffc90420431
ankera <15189120919@163.com>
2023-08-27 10:02:06 +0800

    feat: vue强大插件系统测试

--- src/plugins/test.plugin.ts
+++ src/plugins/test.plugin.ts
@@ -4,7 +4,7 @@ import HelloTest from '../components/HelloTest.vue';
 const plugins = {
     install: (app: App) => {
         app.config.globalProperties.$echo = () => {
-            console.log('全局上注册一个$echo方法');
+            console.log('全局上注册一个$echo方法~~~');
         }
 
         app.component('hello-test', HelloTest)
